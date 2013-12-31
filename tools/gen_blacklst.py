a019f3df27cba325a8a664f189a5311994a3d665
xunzhang <xunzhangthu@gmail.com>
2013-12-31 15:02:04 +0800

    tools added

--- tools/gen_blacklst.py
+++ tools/gen_blacklst.py
@@ -1,5 +1,4 @@
 # -*- coding:utf-8 -*-
-# generate black_lst file
 import sys
 sys.path.append('/var/shire')
 from glob import glob
@@ -15,7 +14,8 @@ user_fav_rdd = dpark.textFile(files, splitSize=64<<20).map(
             lambda line: line.strip().split('\t')
         ).filter(
             # uid, tid, sid
-            lambda val_list: len(val_list)==3 and all([val.isdigit() for val in val_list])
+            lambda val_list: len(val_list)==3 and all([val.isdigit() \
+                    for val in val_list])
         ).map(
             lambda (uid, tid, sid): (int(uid), (tid))
         ).groupByKey(100).filter(
@@ -26,18 +26,44 @@ seed_user_list = user_fav_rdd.map(
         ).collect()
 dseed_user_set = dpark.broadcast(set(seed_user_list))
 
+# user ban tracks
+files = glob(r'/mfs/alg/dbsync/user_banned_track2/*')
+user_ban_rdd = dpark.textFile(files, splitSize=64<<20).map(
+            lambda line: line.strip().split('\t')
+        ).filter(
+            # uid, sid, timestamp
+            lambda val_list: len(val_list)==3 and all([val.isdigit() \
+                    for val in val_list[:2]]) and (int(val_list[0]) \
+                    in dseed_user_set.value)
+        ).map(
+            lambda (uid, sid, timestamp): (int(uid), int(sid))
+        ).groupByKey(100)
+
+# merge fav & ban tracks
+def _merge_track(fav_track_list, ban_track_list):
+    if fav_track_list and ban_track_list:
+        return fav_track_list + ban_track_list
+    else:
+        return fav_track_list if fav_track_list else ban_track_list
+user_fav_rdd = user_fav_rdd.outerJoin(user_ban_rdd, 100).mapValue(
+            lambda (fav_list, ban_list): _merge_track(fav_list, ban_list)
+        )
+
 # track -> artist info
-track_artist_dict = dpark.textFile('/mfs/alg/radio/song_info_v2.data', splitSize=4<<20).map(
+track_artist_dict = dpark.textFile('/mfs/alg/radio/song_info_v2.data',
+        splitSize=4<<20).map(
             lambda line: line.strip().split('\t')
         ).map(
             # tid, new_id, aids, sids, main_subject_id, is_hidden, artist_name, song_name
-            lambda val_list: (int(val_list[1]), [int(aid) for aid in val_list[2].split(',')] if val_list[2] else [])
+            lambda val_list: (int(val_list[0]), [int(aid) for aid in
+                val_list[2].split(',')] if val_list[2] else [])
         ).collectAsMap()
 dtrack_artist_dict = dpark.broadcast(track_artist_dict)
 
-# user ban artists
+# ban artist tracks
 def ban_artist(uid, ban_track_list):
-    fav_artist_set = set([int(tid) for tid, _ in RadioUser.get_user_favorite_artists(uid)])
+    fav_artist_set = set([int(tid) for tid, _ in \
+            RadioUser.get_user_favorite_artists(uid)])
     ban_artist_cnt_dict = {}
     for tid in ban_track_list:
         aid_list = dtrack_artist_dict.value.get(tid, [])
@@ -45,28 +71,17 @@ def ban_artist(uid, ban_track_list):
             ban_artist_cnt_dict[aid] = ban_artist_cnt_dict.get(aid, 0) + 1
     fin_artist_set = set()
     for aid, cnt in ban_artist_cnt_dict.items():
-        if (cnt > 3) and (aid not in fav_artist_set):
+        if (cnt >= 3) and (aid not in fav_artist_set):
             fin_artist_set.add(aid)
     return (uid, fin_artist_set)
-files = glob(r'/mfs/alg/dbsync/user_banned_track2/*')
-user_ban_rdd = dpark.textFile(files, splitSize=64<<20).map(
-            lambda line: line.strip().split('\t')
-        ).filter(
-            # uid, sid, timestamp
-            lambda val_list: len(val_list)==3 and all([val.isdigit() for val in val_list[:2]]) \
-                    and (int(val_list[0]) in dseed_user_set.value)
-        ).map(
-            lambda (uid, sid, timestamp): (int(uid), int(sid))
-        ).groupByKey(100).map(
+user_ban_artist_rdd = user_ban_rdd.map(
             lambda (uid, ban_track_list): ban_artist(uid, ban_track_list)
         )
-
-# banned artist tracks
-banned_artist_list = user_ban_rdd.flatMap(
+banned_artist_list = user_ban_artist_rdd.flatMap(
             lambda (uid, ban_artist_set): ban_artist_set
         ).uniq().collect()
 banned_artist_set = set(banned_artist_list)
-output_file = open('/mfs/user/wuhong/Data/fm/artist_track_dict.csv', 'w')
+output_file = open('artist_track_dict.csv', 'w')
 for aid in banned_artist_set:
     artist_obj = RadioArtist.get(aid)
     if artist_obj:
@@ -74,17 +89,16 @@ for aid in banned_artist_set:
         print >> output_file, '%s:%s' % (aid, sid_str)
 output_file.close()
 
-# banned tracks
-def _(fav_track_list, ban_artist_set):
+# merge artists
+def _merge_artist(fav_track_list, ban_artist_set):
     if fav_track_list and not ban_artist_set:
         return map(str, fav_track_list)
     elif not fav_track_list and ban_artist_set:
         return ['a' + str(aid) for aid in ban_artist_set]
     elif fav_track_list and ban_artist_set:
         return map(str, fav_track_list) + ['a' + str(aid) for aid in ban_artist_set]
-
-user_banned_rdd = user_fav_rdd.outerJoin(user_ban_rdd, 100).mapValue(
-            lambda (fav_track_list, ban_artist_set): _(fav_track_list, ban_artist_set)
+user_banned_rdd = user_fav_rdd.outerJoin(user_ban_artist_rdd, 100).mapValue(
+            lambda (fav_list, ban_list): _merge_artist(fav_list, ban_list)
         ).map(
             lambda (uid, res_list): '%s:%s' % (uid, '|'.join(res_list))
-        ).saveAsCSVFile('/mfs/user/wuhong/tmp/blacklst')
+        ).saveAsCSVFile('blacklst.csv')
---
+++ tools/merge.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python
+import os
+
+os.system('cat ./blacklst.csv/*.csv > blacklst_dict.csv')
